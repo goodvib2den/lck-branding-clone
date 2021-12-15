6698df4cd99baa983b9b609fc552f6f69db5d1b3
eden <gmean7@gmail.com>
2021-12-15 23:49:57 +0900

    FIX Scroll motion fn ERR

--- src/js/bg-scroll.js
+++ src/js/bg-scroll.js
@@ -1,31 +1,26 @@
 function getWindow() {
-  let scrt = window.scrollY;
-  let scrl = window.scrollX;
-  let winh = window.innerHeight;
-  let winw = window.innerWidth;
-
-  const BG_1 = "bg-1";
-  const BG_2 = "bg-2";
-
-  /* sections lines */
-  const body = document.querySelector("body");
-  const logoSection = document.querySelector(".section--logo");
-  const logoSectionT = logoSection.offsetTop;
-  const logoSectionH = logoSection.clientHeight / 2;
-
-  /* scroll fn */
-  window.addEventListener("scroll", function () {
-    // const scrY = window.scrollY;
-
-    if (scrt < logoSectionT + logoSectionH) {
-      body.classList.add(BG_1);
-      body.classList.remove(BG_2);
-    } else if (scrt > logoSectionT + logoSectionH) {
-      body.classList.add(BG_2);
-      body.classList.remove(BG_1);
-    }
-  });
+  scrt = window.scrollY;
+  scrl = window.scrollX;
+  winh = window.innerHeight;
+  winw = window.innerWidth;
 }
+
+/* sections lines */
+const body = document.querySelector("body");
+const logoSection = document.querySelector(".section--logo");
+const logoSectionT = logoSection.offsetTop;
+const logoSectionH = logoSection.clientHeight / 2;
+
+/* scroll fn */
+window.addEventListener("scroll", function () {
+  // const scrY = window.scrollY;
+
+  if (scrt < logoSectionT + logoSectionH) {
+    body.className = "bg-1";
+  } else if (scrt > logoSectionT + logoSectionH) {
+    body.className = "bg-2";
+  }
+});
 
 getWindow();
 window.addEventListener("scroll", getWindow);
