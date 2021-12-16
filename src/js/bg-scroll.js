61d15099a2a4f57e1784071cbb6d2974a0e62354
eden <gmean7@gmail.com>
2021-12-17 03:26:53 +0900

    FIX : 스크롤 배경색상 코드 수정

--- src/js/bg-scroll.js
+++ src/js/bg-scroll.js
@@ -5,20 +5,23 @@ function getWindow() {
   winw = window.innerWidth;
 }
 
-/* sections lines */
-const body = document.querySelector("body");
-const logoSection = document.querySelector(".section--logo");
-const logoSectionT = logoSection.offsetTop;
-const logoSectionH = logoSection.clientHeight / 2;
-
 /* scroll fn */
 window.addEventListener("scroll", function () {
-  // const scrY = window.scrollY;
+  /* sections lines */
+  const body = document.querySelector("body");
+  const logoSection = document.querySelector(".section--logo");
+  const logoSectionT = logoSection.offsetTop;
+  const logoSectionH = logoSection.clientHeight / 2;
+  const logoParallex = document.querySelector(".section--logo-content");
+  const logoParallexT = logoParallex.getBoundingClientRect().top;
+  const logoParallexH = logoParallex.clientHeight / 2;
 
   if (scrt < logoSectionT + logoSectionH) {
     body.className = "bg-1";
   } else if (scrt > logoSectionT + logoSectionH) {
     body.className = "bg-2";
+  } else if (scrt > logoParallexT + logoParallexH) {
+    body.className = "bg-3";
   }
 });
 
